66ddcf8dd43b3cdafb13d535b2dfefc0451ebead
agent200039 <hiroki.simizu@tokyo.supersoftware.co.jp>
2024-04-14 18:35:36 +0900

    first commit - commiting all files to the repository

---
+++ degree.py
@@ -0,0 +1,11 @@
+import math
+ 
+for degree in range(0, 361, 15):
+    angle_radians = math.radians(degree)  # 度をラジアンに変換
+    sin_value = round(math.sin(angle_radians), 2)
+    cos_value = round(math.cos(angle_radians), 2)
+    tan_value = round(math.tan(angle_radians), 2)
+
+    print("sin" + str(degree) + "°の値は" + str(sin_value))
+    print("cos" + str(degree) + "°の値は" + str(cos_value))
+    print("tan" + str(degree) + "°の値は" + str(tan_value))
